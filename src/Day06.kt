d835dd9a76a32b1421822dde20607ff5f1a8179e
TobiasGrahnert <tobias.grahnert@novis-software.de>
2023-12-06 07:38:49 +0100

    solved Day06 part 1 and 2

---
+++ src/Day06.kt
@@ -0,0 +1,37 @@
+fun main() {
+    fun getWaysToWin(times: List<Long>, distances: List<Long>):List<Long> {
+        val results = mutableListOf<Long>()
+        for ( (index, time) in times.withIndex()) {
+            var count = 0L
+            for (i in 1..<time) {
+                if ((time - i) * i > distances[index]) count++
+                else if (count > 0) break
+            }
+            if (count > 0) results.addLast(count)
+        }
+        return results
+    }
+
+    fun part1(input: List<String>): Long {
+        val times = input[0].split("""\s""".toRegex()).filter { it.isNotBlank() }.drop(1).map { it.toLong() }
+        val distances = input[1].split("""\s""".toRegex()).filter { it.isNotBlank() }.drop(1).map { it.toLong() }
+        val waysToWin = getWaysToWin(times, distances)
+        return waysToWin.reduce {acc, i -> acc*i }
+    }
+
+    fun part2(input: List<String>): Long {
+        val times = input[0].split("""\s""".toRegex(), 2).drop(1).map { it.replace("""\s""".toRegex(), "").toLong() }
+        val distances = input[1].split("""\s""".toRegex(), 2).drop(1).map { it.replace("""\s""".toRegex(), "").toLong() }
+        val waysToWin = getWaysToWin(times, distances)
+        return waysToWin.reduce {acc, i -> acc*i }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    check(part1(testInput) == 288L)
+    check(part2(testInput) == 71503L)
+
+    val input = readInput("Day06")
+    part1(input).println()
+    part2(input).println()
+}
