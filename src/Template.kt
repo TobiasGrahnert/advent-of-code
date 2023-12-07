9214f29a584eb96fd399ae8fab5927340e80c84c
TobiasGrahnert <tobias.grahnert@novis-software.de>
2023-12-07 10:02:37 +0100

    updated Template

--- src/Template.kt
+++ src/Template.kt
@@ -1,15 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
 
     val input = readInput("Day01")
     part1(input).println()
